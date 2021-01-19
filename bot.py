eee9c449ebe43db836b3c6d61106f4ba3a8206c1
Richie <richiekim501@gmail.com>
2021-01-19 15:05:47 +1100

    Refactored code to utilise cogs
    Cogs allow codes for certain extensions, such as commands, to be
    edited and dynamically updated without having to make the bot go offline.
    
    Also makes the code more in-line with the open-closed principle.

--- bot.py
+++ bot.py
@@ -5,46 +5,72 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-client = commands.Bot(command_prefix="./", activity=discord.Game(name="./help"), help_command=None)
+client = commands.Bot(command_prefix="./", activity=discord.Game(name="./help"))
+
+def check_if_bot_owner(ctx):
+    return ctx.message.author.id == int(os.getenv("RICHIE"))
 
 def default_embed_template():
     embedded_message = discord.Embed(title="Unital Bot", colour=discord.Colour.teal())
-    embedded_message.set_footer(text="Written by richie#2785", icon_url="https://cdn.discordapp.com/avatars/139204017296375808/9d560b7112b6f3233d323322feb7ca5d.png?size=128")
+    embedded_message.set_footer(text="Written by richie#2785", icon_url=os.getenv("ICON"))
     return embedded_message
 
 @client.event
 async def on_ready():
-    print(f"{client.user} has connected to {len(client.guilds)} server(s)")
+    print(f"{client.user} has connected to {len(client.guilds)} server(s).")
     
 @client.event
 async def on_disconnect():
-    print(f"{client.user} has disconnected")
+    print(f"{client.user} has disconnected.")
 
 @client.event
 async def on_resumed():
-    print(f"{client.user} has reconnected")
+    print(f"{client.user} has reconnected.")
 
 @client.event
 async def on_command_error(ctx, error):
-    await ctx.send(content=f"Error: Invalid command")
-
-@client.command()
-async def help(ctx):
-    embedded_message = default_embed_template()
-
-    args = ctx.message.content.split()
-
-    if len(args) == 1:
-        embedded_message.add_field(name="Bot Commands", value="`./help <command>`")
-        await ctx.send(embed=embedded_message) 
-
-@client.command()
-async def ping(ctx):
-    args = ctx.message.content.split()
-
-    if len(args) > 1:
-        await ctx.send(content=f"Usage: ./ping")
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send(content=f"Error: Command not found.")
+    elif isinstance(error, commands.CheckFailure):
+        await ctx.send(content=f"Error: Command denied. Only the bot's owner can use this command.")
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(content=f"Error: Missing required arguments. Please use the help command for usage.")
     else:
-        await ctx.send(content=f"{round(client.latency*1000)}ms")
+        print(f"Error: {error}")
+        await ctx.send(content=f"Error: {error}")
 
-client.run(os.getenv('TOKEN'))
+@client.command()
+@commands.check(check_if_bot_owner)
+async def load(ctx, extension):
+    client.load_extension(f"cogs.{extension}")
+    await ctx.send(content=f"{extension} has been loaded.")
+
+@client.command()
+@commands.check(check_if_bot_owner)
+async def unload(ctx, extension):
+    print("Starting to unload")
+    client.unload_extension(f"cogs.{extension}")
+    await ctx.send(content=f"{extension} has been unloaded.")
+
+@client.command()
+@commands.check(check_if_bot_owner)
+async def reload(ctx, extension):
+    client.unload_extension(f"cogs.{extension}")
+    client.load_extension(f"cogs.{extension}")
+    await ctx.send(content=f"{extension} has been reloaded.")
+
+# @client.command()
+# async def help(ctx):
+#     embedded_message = default_embed_template()
+
+#     args = ctx.message.content.split()
+
+#     if len(args) == 1:
+#         embedded_message.add_field(name="Bot Commands", value="`./help <command>`")
+#         await ctx.send(embed=embedded_message) 
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith("py"):
+        client.load_extension(f"cogs.{filename[:-3]}")
+
+client.run(os.getenv("TOKEN"))
---
+++ cogs/ping.py
@@ -0,0 +1,19 @@
+import discord 
+
+from discord.ext import commands
+
+class Ping(commands.Cog):
+	def __init__(self, client):
+		self.client = client
+
+	@commands.command()
+	async def ping(self, ctx):
+		args = ctx.message.content.split()
+
+		if len(args) > 1:
+			await ctx.send(content=f"Usage: ./ping")
+		else:
+			await ctx.send(content=f"{round(self.client.latency*1000)}ms")
+
+def setup(client):
+	client.add_cog(Ping(client))
